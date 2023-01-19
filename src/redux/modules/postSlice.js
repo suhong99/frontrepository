8394d96bc9b6d61f45168d888cd2e2c94c84a8d3
suhong99 <topic828@naver.com>
2023-01-19 12:03:32 +0900

    [fix] 병합출동 해결

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -14,10 +14,7 @@ export const __getPost = createAsyncThunk(
   async (payload, thunkAPI) => {
     try {
       const { data } = await http.get("/quiz");
-<<<<<<< HEAD
       console.log(data);
-=======
->>>>>>> Detail
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
